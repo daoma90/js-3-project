8f59e2bf78cd13d8954f22273b1fed82493ceca5
David Öman <Daoma90@gmail.com>
2020-09-29 14:39:46 +0200

    Updates

---
+++ precache-manifest.9b7d1c0b9a6b80c24176139a63af4028.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dbb8feb0f09e2599e7c8b35958eac60",
+    "url": "/js-3-project/index.html"
+  },
+  {
+    "revision": "66bcd24b6d8fea6fce7f",
+    "url": "/js-3-project/static/css/main.47c023e5.chunk.css"
+  },
+  {
+    "revision": "ab6eb1996bd7ee74fa18",
+    "url": "/js-3-project/static/js/2.0a1d4361.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/js-3-project/static/js/2.0a1d4361.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66bcd24b6d8fea6fce7f",
+    "url": "/js-3-project/static/js/main.3ec2e561.chunk.js"
+  },
+  {
+    "revision": "6cbd53fea3434b0044bf",
+    "url": "/js-3-project/static/js/runtime-main.eba357d1.js"
+  }
+]);
